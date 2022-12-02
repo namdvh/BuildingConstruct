c8e828765c1874ba77cdd80d620ce38d0801bb53
Hoài Nam <namhoaidoan15@gmail.com>
2022-12-02 07:44:30 +0700

    add models

---
+++ Data/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Data.Enum;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public Gender? Gender { get; set; }
+        public DateTime? DOB { get; set; }
+        public string? Avatar { get; set; }
+        public Status Status { get; set; }
+        public string? Token { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
