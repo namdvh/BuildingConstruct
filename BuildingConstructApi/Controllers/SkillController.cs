d7071537742e54370e397904e56339cfb30fa85b
Thinh Nguyen <anhthinh.61.got@gmail.com>
2023-01-05 10:36:05 +0700

    Update get all skill

--- BuildingConstructApi/Controllers/SkillController.cs
+++ BuildingConstructApi/Controllers/SkillController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.System.ContractorPosts;
+using Application.System.Skills;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ViewModels.Pagination;
 
 namespace BuildingConstructApi.Controllers
 {
@@ -7,5 +11,32 @@ namespace BuildingConstructApi.Controllers
     [ApiController]
     public class SkillController : ControllerBase
     {
+        private readonly ISkillService _skillService;
+
+        public SkillController(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAll([FromQuery] PaginationFilter request)
+        {
+            dynamic validFilter;
+
+            if (request.FilterRequest == null)
+            {
+                validFilter = new PaginationFilter(request.PageNumber, request.PageSize, request._sortBy, request._orderBy);
+            }
+            else
+            {
+                validFilter = new PaginationFilter(request.PageNumber, request.PageSize, request._sortBy, request._orderBy, request.FilterRequest);
+
+            }
+
+            var result = await _skillService.GetAll(validFilter);
+            return Ok(result);
+        }
     }
 }
--- BuildingConstructApplication/System/Skills/ISkillService.cs
+++ BuildingConstructApplication/System/Skills/ISkillService.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModels.ContractorPost;
+using ViewModels.Pagination;
 
 namespace Application.System.Skills
 {
     public interface ISkillService
     {
+        Task<BasePagination<List<Skill>>> GetAll(PaginationFilter filter);
     }
 }
