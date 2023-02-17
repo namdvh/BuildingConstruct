f684a1b8d0ce660f286c1690b601ced5c1403356
Thinh Nguyen <anhthinh.61.got@gmail.com>
2023-02-17 08:46:57 +0700

    Update product type

--- Data/Entities/ProductType.cs
+++ Data/Entities/ProductType.cs
@@ -1,15 +1,8 @@
-﻿using Data.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Data.Entities
+﻿namespace Data.Entities
 {
     public class ProductType
     {
-        
+
         public int Id { get; set; }
         public string Name { get; set; }
 
