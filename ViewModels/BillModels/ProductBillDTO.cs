6f6e8ef595a3e452c21acc0a2f98d40628ac103f
Hoài Nam <namhoaidoan15@gmail.com>
2023-03-03 16:39:00 +0700

    add GetAllBill

--- ViewModels/BillModels/ProductBillDTO.cs
+++ ViewModels/BillModels/ProductBillDTO.cs
@@ -8,7 +8,10 @@ namespace ViewModels.BillModels
 {
     public class ProductBillDTO
     {
+        public int? Id { get; set; }
         public int ProductId { get; set; }
+
+        public List<ProductBillDetail> ProductBillDetail { get; set; }
         public int? TypeID { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
