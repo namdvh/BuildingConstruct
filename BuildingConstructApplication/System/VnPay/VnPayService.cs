eab6f6cd39b5e2d53a23c515af7781d078037611
Hoài Nam <namhoaidoan15@gmail.com>
2023-03-14 13:29:03 +0700

    fix Payment

--- BuildingConstructApplication/System/VnPay/VnPayService.cs
+++ BuildingConstructApplication/System/VnPay/VnPayService.cs
@@ -38,7 +38,7 @@ namespace Application.System.VnPay
             pay.AddRequestData("vnp_CurrCode", _configuration["Vnpay:CurrCode"]);
             pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(context));
             pay.AddRequestData("vnp_Locale", _configuration["Vnpay:Locale"]);
-            pay.AddRequestData("vnp_OrderInfo", $"{model.Name},{model.UserId}, {model.OrderDescription} {model.Amount}");
+            pay.AddRequestData("vnp_OrderInfo", $"{model.Name} {model.OrderDescription} {model.Amount}");
             pay.AddRequestData("vnp_OrderType", model.OrderType);
             pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
             pay.AddRequestData("vnp_TxnRef", tick);
--- ViewModels/Payment/PaymentRequestDTO.cs
+++ ViewModels/Payment/PaymentRequestDTO.cs
@@ -12,10 +12,7 @@ namespace ViewModels.Payment
         public string OrderDescription { get; set; }
         public string TransactionId { get; set; }
         public string OrderId { get; set; }
-        public string PaymentMethod { get; set; }
         public string PaymentId { get; set; }
-        public bool Success { get; set; }
-        public string Token { get; set; }
         public string VnPayResponseCode { get; set; }
         public string Amount { get; set; }
     }
