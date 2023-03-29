a032a9e5035de00eb9b9cbc76b90fcb0df297ed5
Thinh Nguyen <anhthinh.61.got@gmail.com>
2023-03-29 16:37:20 +0700

    Update

---
+++ BuildingConstructApi/Controllers/testing.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BuildingConstructApi.Controllers
+{
+    [Route("api/testing")]
+    [ApiController]
+    public class testing : ControllerBase
+    {
+        // GET: api/testing
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<testing>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<testing>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<testing>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<testing>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
