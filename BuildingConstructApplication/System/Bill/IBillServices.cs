2dffc5eea8049b36617001fe2e314db0004726ae
Hoài Nam <namhoaidoan15@gmail.com>
2023-04-11 16:06:23 +0700

    modify create bill

--- BuildingConstructApplication/System/Bill/IBillServices.cs
+++ BuildingConstructApplication/System/Bill/IBillServices.cs
@@ -14,7 +14,7 @@ namespace Application.System.Bill
 {
     public interface IBillServices
     {
-        Task<bool> CreateBill(List<BillDTO> requests);
+        Task<BaseResponse<string>> CreateBill(List<BillDTO> requests);
         Task<BasePagination<List<BillDTO>>> GetAllBill(PaginationFilter filter);
 
         Task<BaseResponse<BillDetailDTO>> GetDetail(int billID);
