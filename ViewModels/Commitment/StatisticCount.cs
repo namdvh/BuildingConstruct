5d6467d7e77932704e6524212ee5871e3d67e61f
Thinh Nguyen <anhthinh.61.got@gmail.com>
2023-05-02 07:28:40 +0700

    Update statistic count

--- ViewModels/Commitment/StatisticCount.cs
+++ ViewModels/Commitment/StatisticCount.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ViewModels.Commitment
+﻿namespace ViewModels.Commitment
 {
     public class StatisticCount
     {
-        public int Commitment { get; set; }
-        public int Bill { get; set; }
-        public int Post { get; set; }
         public int Apply { get; set; }
+        public int Bill { get; set; }
+        public int Commitment { get; set; }
+        public int LastApply { get; set; }
+        public int LastBill { get; set; }
+        public int LastCommitment { get; set; }
+        public int LastPost { get; set; }
+        public int Post { get; set; }
     }
 }
